cec350866d69cd78cf7ae0675f64c1abce539cc7
laurent-martinez <martilaurier@gmail.com>
2023-02-14 20:20:25 +0100

    change data values

--- sanityecommerce/sanity.cli.ts
+++ sanityecommerce/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.NEXT_PUBLIC_SANITY_API_KEY,
+    projectId: 'du17da5v',
     dataset: 'production'
   }
 })
