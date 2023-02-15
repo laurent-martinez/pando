838f7278111bd5a294dbaeae08f87e8d409fc8d0
laurent-martinez <martilaurier@gmail.com>
2023-02-15 13:16:40 +0100

    fix bugs v5

--- sanityecommerce/sanity.cli.ts
+++ sanityecommerce/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from '@sanity/cli';
+import defineCliConfig from '@sanity/client';
 
 export default defineCliConfig({
-  api: {
+  
     projectId: 'du17da5v',
     dataset: 'production'
-  }
+  
 })
